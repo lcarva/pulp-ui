3e0b87f8fc66f0ae8ce55db3bcc749f0def5cb41
Luiz Carvalho <lucarval@redhat.com>
2016-09-21 20:48:43 -0400

    Implement rudimentary pagination

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -11,11 +11,57 @@ angular.module('myApp.view1', ['ngRoute'])
 
 .controller('View1Ctrl', ['$http', '$scope', function($http, $scope) {
 
+    var STEP = 20;
+
+    var params = {'criteria': {
+        'limit': STEP,
+        'skip': 0,
+        'sort': [['id', 'ascending']]
+    }};
+
+    var loading = false;
+
+    $scope.repos = [];
+
     function process_repos(response) {
         $scope.repos = response.data || [];
+        loading = false;
     }
 
-    $http.get('/api/v2/repositories/search/', {'params': {'limit': 999}})
-        .then(process_repos);
+    function get_page() {
+        // Clear the list of repos as a workaround for missing
+        // loading indicator.
+        $scope.repos = [];
+        loading = true;
+        $http.post('/api/v2/repositories/search/', params)
+             .then(process_repos);
+    }
 
+    function next_page() {
+        params.criteria.skip += STEP;
+        get_page();
+    }
+
+    function previous_page() {
+        params.criteria.skip -= STEP;
+        get_page();
+    }
+
+    function has_next_page() {
+        return !loading && $scope.repos.length == STEP;
+    }
+
+    function has_previous_page() {
+        return !loading && params.criteria.skip > 0;
+    }
+
+    $scope.config = {'showSelectBox': false}
+
+    $scope.previous_page = previous_page;
+    $scope.next_page = next_page;
+
+    $scope.has_previous_page = has_previous_page;
+    $scope.has_next_page = has_next_page;
+
+    get_page();
 }]);
