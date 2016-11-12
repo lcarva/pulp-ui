f7df279e9c22ff16751e47556155cea9adcd8fb3
Luiz Carvalho <lucarval@redhat.com>
2016-11-11 22:41:24 -0500

    Add tasks page

--- app/tasks/tasks.js
+++ app/tasks/tasks.js
@@ -14,23 +14,41 @@ angular.module('myApp.tasks', []).controller('TasksCtrl',
 
     var full_data_set = false;
 
+    function Task(task) {
+        _.assign(this, _.pick(task,
+            ['task_id', 'task_type', 'state', 'finish_time']
+        ));
+        // pulp.server.controllers.repository.download_deferred -> Download Deferred
+        this.title = _.chain(this.task_type.split('.')).last().startCase().value();
+        this.tags = [];
+        this.repos = [];
+        task.tags.forEach(function(tag) {
+            if (tag.startsWith('pulp:action:')) { return; }
+            if (tag.startsWith('pulp:repository_distributor:')) { return; }
+
+            if (tag.startsWith('pulp:repository:')) {
+                this.repos.push(tag.replace('pulp:repository:', ''));
+                return;
+            }
+
+            this.tags.push(tag);
+
+        }, this);
+    }
+
     $scope.items = [];
 
     function process_repos(response) {
         var new_items = response.data || [];
-        // Augment repositories to easily check for empty ones
-        new_items.forEach(function(item) {
-            var total_content_units = 0;
-            for (var unit in item.content_unit_counts) {
-                total_content_units += item.content_unit_counts[unit];
-            }
 
-            item.total_content_units = total_content_units;
+        new_items.forEach(function(item) {
+            $scope.items.push(new Task(item));
         });
+
         if (new_items.length < STEP) {
             full_data_set = true;
         }
-        Array.prototype.push.apply($scope.items, new_items);
+
         $scope.loading = false;
     }
 
